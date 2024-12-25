037f0991edf5a28db554b1c2ccf8b1562f46daa9
MSA63 <manjunathyadavs199@gmai.com>
2024-12-25 14:26:58 +0530

    Design Patterns

---
+++ src/com/dassault/systemes/creational/Adapter/XPay.java
@@ -0,0 +1,16 @@
+package src.com.dassault.systemes.creational.Adapter;
+
+public interface XPay {
+    public String getCreditCardNo();
+    public String getCustomerName();
+    public String getCardExpMonth();
+    public String getCardExpYear();
+    public Short getCardCVVNo();
+    public Double getAmount();
+    public void setCreditCardNo(String creditCardNo);
+    public void setCustomerName(String customerName);
+    public void setCardExpMonth(String cardExpMonth);
+    public void setCardExpYear(String cardExpYear);
+    public void setCardCVVNo(Short cardCVVNo);
+    public void setAmount(Double amount);
+}
